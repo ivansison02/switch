4e5b75b7c296c20b9d50b414133015b9f0cbd2e0
Ivan <ivan_sison02@ymail.com>
2017-11-08 14:47:21 +0800

    first commit
    Commit

--- app/src/main/java/mgi/ivan_sison/switcher/MainActivity.java
+++ app/src/main/java/mgi/ivan_sison/switcher/MainActivity.java
@@ -3,6 +3,8 @@ package mgi.ivan_sison.switcher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import mgi.ivan_sison.ifswitch.Switch;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
